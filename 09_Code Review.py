f177a4bb349f5162bdf0f668775d4ce5e6fa58a5
Jess McKenzie <spacemonkeys@internode.on.net>
2021-08-27 10:42:09 +1000

    Add bad code for later refactoring

---
+++ 09_Code Review.py
@@ -0,0 +1,17 @@
+def main():
+    """get and store names and breeds of dogs in dictionary"""
+    totalDog = int(input("How many Dogs do you have?: "))
+    dogDict = {}
+
+    
+    if totalDog > 0:
+        for i in range(totalDog):
+            name = input("Dog {}'s Name?: ".format(i))
+            breed = input("{}'s breed?: ".format(name))
+            dogDict[name] = breed
+    print(dogDict)
+
+    # TODO: fix naming
+    # TODO: fix formatting
+    # TODO: start counting dogs at 1, not 0
+main()
